ac2171ebce0bc04ed4d7be291a96e84c7d41e238
KelanLindahl <kelanlindahl5@gmail.com>
2022-07-14 13:42:11 -0400

    Herding Decision Boilerplate added

---
+++ zombieGame-AiFinal/Assets/Scripts/DTStuff/DecisionHerding.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DecisionHerding: Decision
+{
+    public override Action GetBranch()
+    {
+        // TODO: Add branch logic here
+        return null;
+    }
+}
