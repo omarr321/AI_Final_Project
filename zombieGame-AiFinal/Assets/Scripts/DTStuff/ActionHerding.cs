0c7db9e5c33da307141dc6c48fa0561433e9f30a
KelanLindahl <kelanlindahl5@gmail.com>
2022-08-02 13:27:52 -0400

    pushing actionherding

--- zombieGame-AiFinal/Assets/Scripts/DTStuff/ActionHerding.cs
+++ zombieGame-AiFinal/Assets/Scripts/DTStuff/ActionHerding.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class ActionHerding: Action
 {
+    Collider[] zombeanSphere;
+    int i=0;
+    public static List<GameObject> Test = new List<GameObject>();
     public override DTNode MakeDecision()
     {
         return GetComponent<DecisionHerding>().GetBranch();
@@ -11,6 +14,21 @@ public class ActionHerding: Action
 
     public override void LateUpdate()
     {
-        //TODO: Write the code here so that the zombie heads towards the cloest zombies
+        int count = 0;
+        zombeanSphere=Physics.OverlapSphere(transform.position, 0.5f);
+        foreach (var zombeanNeighbor in zombeanSphere)
+        {
+            if (zombeanNeighbor.tag=="clickable"){
+                count++;
+                zombeanNeighbor.transform.position=Vector3.MoveTowards(zombeanNeighbor.transform.position, transform.position, 1.0f);
+            }
+        }
+
+        if (count > 3) {
+           zombeanNeighbor.transform.position=Vector3.MoveTowards(zombeanNeighbor.transform)
+        } else {
+           
+            //TODO: GO to cloest zombean
+        }
     }
 }
